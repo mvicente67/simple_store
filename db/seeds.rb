5e7d6e90df241048bb66024c38dc93ff55573b99
Mark Vicente <mark.r.vicente@gmail.com>
2022-02-16 21:12:29 -0600

    Seeded database with fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Product.delete_all
+
+676.times do
+  Product.create(
+    title:          Faker::Food.ingredient,
+    price:          Faker::Commerce.price,
+    stock_quantity: Faker::Number.between(from: 1, to: 100)
+  )
+
+  puts "Created #{Product.count} Products"
+end
